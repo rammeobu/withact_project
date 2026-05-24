3364ef37e9185a7a0c589b7809ba9d287fb496a7
rammeobu <wnsldjtksid@naver.com>
2026-05-24 22:59:32 +0900

    배포준비

--- src/main/java/com/example/capstone/service/UserService.java
+++ src/main/java/com/example/capstone/service/UserService.java
@@ -6,7 +6,7 @@ import com.example.capstone.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import tools.jackson.databind.util.RecordUtil;
+
 
 
 @Service
